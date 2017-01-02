7ad67ccebd9889923fd93a1526771b540842d116
Kevin Kang <kevink97@cs.washington.edu>
2017-01-01 18:45:46 -0800

    added exercise

---
+++ exercises/Variables.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var a = 15 // 1
+var b = 5  // 2
+var c = a + b // 3
+var d = 9 * 5 // 4
+var e = a - b // 5
+var f = a / b // 6
+var g = 1; var g2 = 2; var g3 = 3 // 7
+let h1 = 1; let h2 = 2; let h3 = 3 // 8
+var i : Double // 9
+var j : Int = 423 // 10
+var k = "Hello" // 11
+var l = "World" // 12
+var m = k + l // 13
+var n = "\(k)\(l)" // 14
