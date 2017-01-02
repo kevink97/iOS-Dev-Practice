2eb610b5786d797b6966b76a532b2faec5e60120
Kevin Kang <kevink97@cs.washington.edu>
2017-01-01 19:40:04 -0800

    added Functions exercises

---
+++ exercises/Functions.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func add(first : Double, second : Double) -> Double {
+    return first + second
+}
+
+func subtract(first : Int, second : Int) -> Int {
+    return first - second
+}
+
+func multiply(first : Float, second : Float) -> Float {
+    return first * second
+}
+
+func divide(first : Double, second : Double) -> Double {
+    return first / second
+}
+
+add(first: 1, second: 2)
+subtract(first: 3, second: 5)
+multiply(first: 1.234, second: 3.42)
+divide(first: 3.2, second: 0.2)
