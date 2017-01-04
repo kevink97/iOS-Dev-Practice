c0829678366daa2939d368cda8da7b92a1275f3c
Kevin Kang <kevink97@cs.washington.edu>
2017-01-03 21:47:42 -0800

    added additional functionality (buy,notifbar)

--- MiraclePill/MiraclePill/ViewController.swift
+++ MiraclePill/MiraclePill/ViewController.swift
@@ -14,8 +14,32 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     @IBOutlet weak var statePickerBtn: UIButton!
     @IBOutlet weak var statePicker: UIPickerView!
     
-    let states = ["Alaska", "Arkansas", "Alabama", "California", "Maine", "New York"]
+    @IBOutlet weak var pillImg: UIImageView!
+    @IBOutlet weak var pillName: UILabel!
+    @IBOutlet weak var pillCost: UILabel!
+    @IBOutlet weak var bar: UIView!
+    @IBOutlet weak var nameText: UILabel!
+    @IBOutlet weak var nameInput: UITextField!
+    @IBOutlet weak var streetText: UILabel!
+    @IBOutlet weak var streetInput: UITextField!
+    @IBOutlet weak var cityText: UILabel!
+    @IBOutlet weak var cityInput: UITextField!
+    @IBOutlet weak var stateText: UILabel!
+    @IBOutlet weak var stateBtn: UIButton!
     
+    
+    @IBOutlet weak var countryText: UILabel!
+    @IBOutlet weak var countryInput: UITextField!
+    @IBOutlet weak var zipcodeText: UILabel!
+    @IBOutlet weak var zipcodeInput: UITextField!
+    
+    @IBOutlet weak var buynowBtn: UIButton!
+    
+    @IBOutlet weak var successImg: UIImageView!
+    
+    
+    let states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
+        "Delaware", "Florida", "Georgia"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +54,30 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     }
 
     @IBAction func stateBtnPressed(_ sender: Any) {
-        statePicker.isHidden = false
+        toggleHidden()
+    }
+    
+    @IBAction func buynowBtnPressed(_ sender: Any) {
+        let allItems : [UIView] = [statePickerBtn, statePicker,
+                                   pillImg, pillName, pillCost,
+                                   bar, nameText, nameInput,
+                                   streetText, streetInput,
+                                   cityText, cityInput,
+                                   stateText, stateBtn,
+                                   countryText, countryInput,
+                                   zipcodeText, zipcodeInput,
+                                   buynowBtn]
+        for item in allItems {
+            if !item.isHidden {
+                item.isHidden = true
+            }
+        }
+        successImg.isHidden = false
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return successImg != nil  && !successImg.isHidden
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -47,7 +94,15 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         statePickerBtn.setTitle(states[row], for: UIControlState.normal)
-        statePicker.isHidden = true
+        toggleHidden()
+    }
+    
+    func toggleHidden() {
+        statePicker.isHidden = !statePicker.isHidden
+        countryText.isHidden = !countryText.isHidden
+        countryInput.isHidden = !countryInput.isHidden
+        zipcodeText.isHidden = !zipcodeText.isHidden
+        zipcodeInput.isHidden = !zipcodeInput.isHidden
     }
 }
 
