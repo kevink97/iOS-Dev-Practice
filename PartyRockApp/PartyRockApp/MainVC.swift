64e02f3b534ce7de82733b432cd28d296c19d225
Kevin Kang <kevink97@cs.washington.edu>
2017-01-09 23:31:27 -0800

    added partyrockapp with initial desigin

---
+++ PartyRockApp/PartyRockApp/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Kevin Kang on 1/9/17.
+//  Copyright © 2017 Kevin Kang. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
