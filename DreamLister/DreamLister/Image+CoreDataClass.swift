d2c2d6af62ed8de62e5aba117dd1167446fae764
Kevin Kang <kevink97@cs.washington.edu>
2017-01-12 01:01:15 -0800

    added data model using core data for DreamLister App

---
+++ DreamLister/DreamLister/Image+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Image+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Kevin Kang on 1/12/17.
+//  Copyright © 2017 Kevin Kang. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Image)
+public class Image: NSManagedObject {
+
+}
---
+++ DreamLister/DreamLister/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Kevin Kang on 1/12/17.
+//  Copyright © 2017 Kevin Kang. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
